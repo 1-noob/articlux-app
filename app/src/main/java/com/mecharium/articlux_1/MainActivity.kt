a67e698da58fffb6c9c5d9f6d606c194e4432cf7
1-noob <the.noob.master.286@gmail.com>
2026-02-14 15:01:10 +0530

    added ModalBottomSheet

--- app/src/main/java/com/mecharium/articlux_1/MainActivity.kt
+++ app/src/main/java/com/mecharium/articlux_1/MainActivity.kt
@@ -9,12 +9,21 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.clickable
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.Alignment
@@ -26,6 +35,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.mecharium.articlux_1.ui.theme.Articlux_1Theme
 import com.mecharium.articlux_1.ui.theme.Burbank
+import kotlinx.coroutines.launch
 
 
 
@@ -47,8 +57,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StartScanButton(modifier: Modifier = Modifier) {
+    
+    var showBottomSheet by remember{ mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+
+    
     Box(
         modifier = modifier,
         contentAlignment = Alignment.Center
@@ -66,7 +83,7 @@ fun StartScanButton(modifier: Modifier = Modifier) {
                     )
                 )
                 .clickable {
-                    // connect to backend!
+                    showBottomSheet = true
                 },
             contentAlignment = Alignment.Center
         ) {
@@ -79,6 +96,28 @@ fun StartScanButton(modifier: Modifier = Modifier) {
                 textAlign = TextAlign.Center
             )
         }
+
+        // Bottom Sheet
+        if (showBottomSheet){
+            ModalBottomSheet(
+                onDismissRequest = { showBottomSheet = false },
+                sheetState = sheetState
+            ) {
+                Box(
+                    modifier = Modifier
+                        .height(200.dp)
+                        .fillMaxWidth()
+                        .padding(24.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Button Pressed",
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+                }
+            }
+        }
+
     }
 }
 

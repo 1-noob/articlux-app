3b75079f955ffd383748e386b5cc9c20e1d8eac4
1-noob <the.noob.master.286@gmail.com>
2026-02-15 21:37:42 +0530

    Basic Layout complete

--- app/src/main/java/com/mecharium/articlux_1/MainActivity.kt
+++ app/src/main/java/com/mecharium/articlux_1/MainActivity.kt
@@ -4,39 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.clickable
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.rememberModalBottomSheetState
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import com.mecharium.articlux_1.ui.screens.HomeScreen
 import com.mecharium.articlux_1.ui.theme.Articlux_1Theme
-import com.mecharium.articlux_1.ui.theme.Burbank
-import kotlinx.coroutines.launch
 
 
 
@@ -51,76 +20,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun StartScanButton(modifier: Modifier = Modifier) {
-    
-    var showBottomSheet by remember{ mutableStateOf(false) }
-    val sheetState = rememberModalBottomSheetState()
-    val scope = rememberCoroutineScope()
-
-    
-    Box(
-        modifier = modifier,
-        contentAlignment = Alignment.Center
-    ) {
-        Box(
-            modifier = Modifier
-                .width(250.dp)
-                .height(68.dp)
-                .background(
-                    brush = Brush.linearGradient(
-                        colors = listOf(
-                            Color(0xFFFEFC41),
-                            Color(0xFFFFFFC6)
-                        )
-                    )
-                )
-                .clickable {
-                    showBottomSheet = true
-                },
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "SCAN",
-                color = Color.Black,
-                fontFamily = Burbank,
-                fontWeight = FontWeight.W600,
-                fontSize = 38.sp,
-                textAlign = TextAlign.Center
-            )
-        }
-
-        // Bottom Sheet
-        if (showBottomSheet){
-            ModalBottomSheet(
-                onDismissRequest = { showBottomSheet = false },
-                sheetState = sheetState
-            ) {
-                Box(
-                    modifier = Modifier
-                        .height(200.dp)
-                        .fillMaxWidth()
-                        .padding(24.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(
-                        text = "Button Pressed",
-                        style = MaterialTheme.typography.headlineMedium
-                    )
-                }
-            }
-        }
-
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewStartScanButton() {
-    Articlux_1Theme {
-        StartScanButton(modifier = Modifier.fillMaxSize())
-    }
-}
-
--- app/src/main/java/com/mecharium/articlux_1/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/mecharium/articlux_1/ui/screens/HomeScreen.kt
@@ -2,17 +2,56 @@ package com.mecharium.articlux_1.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.ExperimentalMaterial3Api
+import com.mecharium.articlux_1.ui.components.PrimaryButton
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(){
+
+    val sheetState = rememberModalBottomSheetState()
+    var showBottomSheet by remember { mutableStateOf(false) }
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "This is the Home Screen")
+        PrimaryButton(
+            text = "SCAN",
+            onClick = {
+                // Scan the feed for new articles
+                showBottomSheet = true
+            },
+            modifier = Modifier.padding(horizontal = 24.dp)
+        )
     }
+
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { showBottomSheet = false },
+            sheetState = sheetState
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
+            ) {
+                Text("Button Pressed!")
+
+                Spacer(Modifier.height(16.dp))
+
+                PrimaryButton(
+                    // Change to Proceed (Synchronising articles into DB)
+                    text = "Close",
+                    onClick = { showBottomSheet = false }
+                )
+
+            }
+        }
+    }
+
 }

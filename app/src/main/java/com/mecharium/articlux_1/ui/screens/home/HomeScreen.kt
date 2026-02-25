b22cfcebce154a3c5043fa2bb6e43d52826f3874
1-noob <the.noob.master.286@gmail.com>
2026-02-25 22:47:23 +0530

    updated HomeScreen code

--- app/src/main/java/com/mecharium/articlux_1/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/mecharium/articlux_1/ui/screens/home/HomeScreen.kt
@@ -1,2 +1,42 @@
 package com.mecharium.articlux_1.ui.screens.home
 
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.mecharium.articlux_1.data.model.Article
+import com.mecharium.articlux_1.ui.components.ArticleCard
+
+
+
+
+@Composable
+fun HomeScreen(){
+    val sampleArticle = Article(
+        title = "New News Article",
+        status = "pending",
+        rating = 3,
+        category = "test"
+    )
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            ArticleCard(
+                article = sampleArticle,
+                onClick = {
+                    // do nothing
+                }
+            )
+        }
+    }
+}

ca14c1cf214536801fe26ee3ab7c71db6bdc30de
1-noob <the.noob.master.286@gmail.com>
2026-03-02 22:42:27 +0530

    created a list of existing categories

---
+++ app/src/main/java/com/mecharium/articlux_1/ui/review/CategoryProvider.kt
@@ -0,0 +1,17 @@
+package com.mecharium.articlux_1.ui.review
+
+object CategoryProvider {
+
+    val categories = listOf(
+        "Art & Culture",
+        "Beyond trending",
+        "Current Affairs",
+        "Ethics simplified",
+        "Upsc issue at a glance",
+        "Knowledge nugget",
+        "Mains Answer Writing",
+        "Upsc key",
+        "World this week"
+    )
+
+}

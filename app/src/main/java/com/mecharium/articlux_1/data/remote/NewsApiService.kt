d8183f3dcecebd6b100c72f0715a951015065bd9
1-noob <the.noob.master.286@gmail.com>
2026-03-05 22:46:06 +0530

    defined the API interface for receiving paginated data

--- app/src/main/java/com/mecharium/articlux_1/data/remote/NewsApiService.kt
+++ app/src/main/java/com/mecharium/articlux_1/data/remote/NewsApiService.kt
@@ -1,8 +1,11 @@
 package com.mecharium.articlux_1.data.remote
 
+import androidx.core.location.LocationRequestCompat
+import com.mecharium.articlux_1.data.model.ArticleResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 
 interface NewsApiService {
@@ -15,4 +18,10 @@ interface NewsApiService {
 
     @POST("api/proceed")
     suspend fun proceed(): Response<ProceedResponse>
+
+    @GET("api/get_articles")
+    suspend fun getArticles(
+        @Query("page") page: Int,
+        @Query("limit") limit: Int = 60
+    ) : ArticleResponse
 }

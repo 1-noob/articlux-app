d767e03a5a2d2122c65c473ae409f4a8e4866829
1-noob <the.noob.master.286@gmail.com>
2026-03-10 23:38:36 +0530

    Basic styling on TopAppBar title text

--- app/src/main/java/com/mecharium/articlux_1/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/mecharium/articlux_1/ui/screens/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -15,6 +16,10 @@ import androidx.compose.material3.VerticalDivider
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+
+import com.mecharium.articlux_1.ui.theme.TomorrowFont
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,14 +32,27 @@ fun HomeScreen() {
         topBar = {
             MediumTopAppBar(
                 title = {
-                    Text ("Articlux")
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Articlux",
+                            modifier = Modifier.padding(start = 40.dp),
+                            fontFamily = TomorrowFont,
+                            fontWeight = FontWeight.SemiBold,
+                            fontStyle = FontStyle.Italic,
+                            fontSize = 26.sp,
+                            color = Color.Gray
+                        )
+                    }
                 }
             )
         }
     ) { paddingValues ->
 
         Row(
-            modifier = Modifier.fillMaxSize().padding(paddingValues)
+            modifier = Modifier.fillMaxSize().padding(paddingValues).padding(8.dp)
         ) {
             NavigationRail(
                 modifier = Modifier.padding(horizontal = 10.dp).width(60.dp)
@@ -90,7 +108,9 @@ fun HomeScreen() {
             }
 
             // Divider
-            VerticalDivider()
+            VerticalDivider(
+                modifier = Modifier.padding(horizontal = 8.dp)
+            )
 
             // Main Content Area
             Box(

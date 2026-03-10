16c7894e9fd624dbab3b0158396cdec18b63c088
1-noob <the.noob.master.286@gmail.com>
2026-03-10 23:37:38 +0530

    created font variables

--- app/src/main/java/com/mecharium/articlux_1/ui/theme/Type.kt
+++ app/src/main/java/com/mecharium/articlux_1/ui/theme/Type.kt
@@ -2,9 +2,13 @@ package com.mecharium.articlux_1.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.sp
+
+import com.mecharium.articlux_1.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,20 +19,19 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+)
+
+
+// Tomorrow Medium Italic font
+val TomorrowFont = FontFamily(
+    Font(
+        R.font.tomorrow_medium_italic,
+        weight = FontWeight.Medium,
+        style = FontStyle.Italic
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    Font(
+        R.font.tomorrow_semibold_italic,
+        weight = FontWeight.SemiBold,
+        style = FontStyle.Italic
     )
-    */
 )

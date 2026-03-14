17ad28ad25fe5104d94131b206de5caafb04f438
1-noob <the.noob.master.286@gmail.com>
2026-03-14 23:29:27 +0530

    added the api endpoint to receive new articles

--- app/src/main/java/com/mecharium/articlux_1/data/remote/NewsApiService.kt
+++ app/src/main/java/com/mecharium/articlux_1/data/remote/NewsApiService.kt
@@ -3,8 +3,11 @@ package com.mecharium.articlux_1.data.remote
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 import com.mecharium.articlux_1.data.model.ProceedResponse
+import com.mecharium.articlux_1.data.model.PaginatedArticles
+
 
 interface NewsApiService {
     @POST("api/scan")
@@ -12,4 +15,9 @@ interface NewsApiService {
 
     @POST("api/proceed")
     suspend fun proceed() : Response<ProceedResponse>
+
+    @GET("api/get_articles")
+    suspend fun getArticles(
+        @Query("page") page: Int
+    ): PaginatedArticles
 }

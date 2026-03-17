b539c98d9b252d5d4571df409ac06574c1001610
1-noob <the.noob.master.286@gmail.com>
2026-03-17 23:17:56 +0530

    added the endpoint for reviewing each article

--- app/src/main/java/com/mecharium/articlux_1/data/remote/NewsApiService.kt
+++ app/src/main/java/com/mecharium/articlux_1/data/remote/NewsApiService.kt
@@ -21,4 +21,11 @@ interface NewsApiService {
     suspend fun getArticles(
         @Query("page") page: Int
     ): PaginatedArticles
+
+    @POST("api/review")
+    suspend fun review(
+        @Query("action") action: String,
+        @Query("url") url: String? = null,
+        @Query("category") category: String? =null
+    ) : ReviewResponse
 }
